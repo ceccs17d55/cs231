4273481032fdd7a3254c1955e60f54d96347245b
ceccs17d55 <43598233+ceccs17d55@users.noreply.github.com>
2018-11-30 11:48:42 +0530

    Update 33_binart tree array.c

--- 33_binart tree array.c
+++ 33_binart tree array.c
@@ -142,3 +142,97 @@ int main() {
 	initialise();
 	showMenu();
 }
+/*
+Output
+[?1h=$ ./33_binary_tree_using_array 
+[?1l>
+	Binary Tree
+1. Insert
+2. Search
+3. Delete
+4. Print
+5. Exit
+Enter your choice : 1
+Enter data to be inserted : 1
+Successfully inserted
+
+	Binary Tree
+1. Insert
+2. Search
+3. Delete
+4. Print
+5. Exit
+Enter your choice : 1
+Enter data of the parent node : 2
+Key not found
+	Binary Tree
+1. Insert
+2. Search
+3. Delete
+4. Print
+5. Exit
+Enter your choice : 1
+Enter data of the parent node : 1
+Enter data to be inserted : 2
+Where do you want to enter the data (L/R): l
+Successfully inserted
+
+	Binary Tree
+1. Insert
+2. Search
+3. Delete
+4. Print
+5. Exit
+Enter your choice : 1
+Enter data of the parent node : 1
+Enter data to be inserted : 3
+Where do you want to enter the data (L/R): r
+Successfully inserted
+
+	Binary Tree
+1. Insert
+2. Search
+3. Delete
+4. Print
+5. Exit
+Enter your choice : 1
+Enter data of the parent node : 1
+Enter data to be inserted : 2 3
+Where do you want to enter the data (L/R): r
+Node not empty. Insertion failed
+
+	Binary Tree
+1. Insert
+2. Search
+3. Delete
+4. Print
+5. Exit
+Enter your choice : 4
+ 2 1 3
+	Binary Tree
+1. Insert
+2. Search
+3. Delete
+4. Print
+5. Exit
+Enter your choice : 3
+Enter data of the node to be deleted : 1
+Successfully deleted
+
+	Binary Tree
+1. Insert
+2. Search
+3. Delete
+4. Print
+5. Exit
+Enter your choice : 4
+ 2 3
+	Binary Tree
+1. Insert
+2. Search
+3. Delete
+4. Print
+5. Exit
+Enter your choice : 5
+
+*/
